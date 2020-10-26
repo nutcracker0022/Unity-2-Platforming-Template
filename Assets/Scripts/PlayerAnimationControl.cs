9630837a46eb4bb43581707f12f5c9237b2a678b
Suraj <surajss@uci.edu>
2020-10-25 22:50:20 -0700

    Finaling Step

--- Assets/Scripts/PlayerAnimationControl.cs
+++ Assets/Scripts/PlayerAnimationControl.cs
@@ -6,10 +6,18 @@ public class PlayerAnimationControl : MonoBehaviour
 {
     [SerializeField]
     private SpriteRenderer playerSprite;
-    //[SerializeField]
-    //private Rigidbody2D rb;
+    [SerializeField]
+    private Animator playerAnimator;
+    [SerializeField]
+    private Rigidbody2D rb;
 
     private bool movingRight = false;
+
+    public float idleThreshold = 0.25f;
+    public float jumpThreshold = 0.5f;
+
+    public float rayStart = 0.45f;
+    public float rayLength = 0.35f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +26,14 @@ public class PlayerAnimationControl : MonoBehaviour
         {
             playerSprite = GetComponentInChildren<SpriteRenderer>();
         }
-        //if (rb == null)
-        //{
-        //    rb = GetComponent<Rigidbody2D>();
-        //}
+        if (playerAnimator == null)
+        {
+            playerAnimator = GetComponentInChildren<Animator>();
+        }
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
     }
 
     // Update is called once per frame
@@ -38,5 +50,22 @@ public class PlayerAnimationControl : MonoBehaviour
             playerSprite.flipX = true;
             movingRight = false;
         }
+        UpdateAnimationParameters();
     }
+
+    private void UpdateAnimationParameters()
+    {
+        //Debug.Log($"Idle? {Mathf.Abs(rb.velocity.x) < idleThreshold}");
+        playerAnimator.SetBool("Idling", Mathf.Abs(rb.velocity.x) < idleThreshold);
+
+        //Debug.Log($"Jump Up? {rb.velocity.y > jumpThreshold}");
+        playerAnimator.SetBool("MoveUpwards", rb.velocity.y > jumpThreshold);
+        
+
+        //Debug.DrawRay(this.transform.position + (Vector3)Vector2.down * rayStart, Vector2.down * (rayStart + rayLength));
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position + (Vector3)Vector2.down * rayStart, Vector2.down, rayLength);
+        //Debug.Log(hit.transform);
+        playerAnimator.SetBool("Grounded", hit);
+    }
+
 }
