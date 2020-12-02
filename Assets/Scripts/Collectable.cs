b3bfad452ec004732848286191a57d946fff1be0
Suraj <surajss@uci.edu>
2020-12-02 11:23:04 -0800

    Added Collectable and Coin

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Collectable : MonoBehaviour
+{
+    public string collectableName;
+    public string description;
+    public GameObject player;
+    public abstract void Use();
+}
