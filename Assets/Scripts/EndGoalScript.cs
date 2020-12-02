0c5ceaf848e55d5359a25303684020112d5005ea
Suraj <surajss@uci.edu>
2020-12-02 12:23:29 -0800

    Added back EndGoalScript

---
+++ Assets/Scripts/EndGoalScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGoalScript : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent OnPlayerFinish;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnPlayerFinish?.Invoke();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        OnPlayerFinish.RemoveAllListeners();
+    }
+}
