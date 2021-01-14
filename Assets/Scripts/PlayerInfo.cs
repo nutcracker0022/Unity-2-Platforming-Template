44a413d27addd23378d114d86430d214acdb7da5
SurajSSingh <surajss@uci.edu>
2021-01-13 19:53:56 -0800

    Fixed PlayerInfo Prefab

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 100;
+    [SerializeField]
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += NewLevelLoaded;
+
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= NewLevelLoaded;
+    }
+
+    void NewLevelLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PlayerManagerScript player = GameObject.FindWithTag("Player")?.GetComponent<PlayerManagerScript>();
+
+        if (player != null)
+        {
+            player.health = health;
+            player.score = score;
+
+            player.OnHealthChange.AddListener(HealthChange);
+            player.OnScoreChange.AddListener(ScoreChange);
+        }
+    }
+
+    void HealthChange(int newHealth)
+    {
+        health = newHealth;
+    }
+
+    void ScoreChange(int newScore)
+    {
+        score = newScore;
+    }
+}
